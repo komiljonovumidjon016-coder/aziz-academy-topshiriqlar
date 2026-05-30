eac02d4cf7c762716e24c68ed6203ccd621efc7d
komiljonovumidjon016-coder <komiljonovumidjon016@gmail.com>
2026-05-30 12:18:37 +0500

    Add AI task: Kitob do'konidagi kitoblar sonini hisoblash dasturi

---
+++ ai_topshiriqlar/Kitob_dokonidagi_kitoblar_sonini_hisoblash_dasturi.py
@@ -0,0 +1,11 @@
+# Kitob do'konidagi kitoblar sonini hisoblash dasturi
+# Kurs: IT Dasturlash
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+kitob_soni = int(input())
+kitob_turlari_soni = int(input())
+kitoblar_soni = list(map(int, input().split()))
+print(sum(kitoblar_soni))
