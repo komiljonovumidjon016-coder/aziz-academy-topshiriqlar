540cef5bda082043b57b12435f8081c0bc689a81
komiljonovumidjon016-coder <komiljonovumidjon016@gmail.com>
2026-05-30 12:14:08 +0500

    Add AI task: Futbolchining o'rtacha gol urish ko'rsatkichini hisoblash dasturi

---
+++ ai_topshiriqlar/Futbolchining_ortacha_gol_urish_korsatkichini_hisoblash_dast.py
@@ -0,0 +1,8 @@
+# Futbolchining o'rtacha gol urish ko'rsatkichini hisoblash dasturi
+# Kurs: IT Dasturlash
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = input().split()
+print(int(a[2])/int(a[1]))
