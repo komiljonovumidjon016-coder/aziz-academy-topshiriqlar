b5f0e722024da9d161a8f6b3b52d163f0068c8ed
komiljonovumidjon016-coder <komiljonovumidjon016@gmail.com>
2026-06-01 08:59:52 +0500

    Add AI task: Kutubxona kitoblarini ro'yxatga olish

---
+++ ai_topshiriqlar/Kutubxona_kitoblarini_royxatga_olish.py
@@ -0,0 +1,14 @@
+# Kutubxona kitoblarini ro'yxatga olish
+# Kurs: IT Dasturlash
+# Mavzu: 2-mavzu: Birinchi dastur, print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+while True:
+    kitob_nomi = input()
+    if kitob_nomi == 'stop':
+        break
+    muallif = input()
+    narx = input()
+    print(kitob_nomi, muallif, narx, sep=", ")
