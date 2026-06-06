04250d88afbd55fa0e449a9020ab930201ab9249
komiljonovumidjon016-coder <komiljonovumidjon016@gmail.com>
2026-06-06 09:01:22 +0500

    Add AI task: Mehmonxona bandlik darajasini hisoblash dasturi

---
+++ ai_topshiriqlar/Mehmonxona_bandlik_darajasini_hisoblash_dasturi.py
@@ -0,0 +1,10 @@
+# Mehmonxona bandlik darajasini hisoblash dasturi
+# Kurs: IT Dasturlash
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+xonalar_soni = int(input())
+band_xonalar_soni = int(input())
+print(band_xonalar_soni * 100 / xonalar_soni)
