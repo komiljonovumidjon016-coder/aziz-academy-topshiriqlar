12b2ee22edfec61ab893d5e0a53bbd5b78c5ff39
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:09:14 +0500

    Add: 9) Menyu: transport [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/9__Menyu__transport.py
@@ -0,0 +1,11 @@
+# t
+# car -> "Drive"
+# bus -> "Ride"
+# aks holda "Walk"
+t = input().strip().lower()
+if t == "car":
+    print("Drive")
+elif t == "bus":
+    print("Ride")
+else:
+    print("Walk")
