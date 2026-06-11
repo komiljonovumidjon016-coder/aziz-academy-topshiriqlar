8510984297f9294ebb03ed5000f70b7f1411278b
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:21:58 +0500

    Add: 15) Uchta shartli kirish [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Ichma-ich_shartlar__nested_if_/15__Uchta_shartli_kirish.py
@@ -0,0 +1,11 @@
+r, a, = input().split()
+if r == "admin":
+    if a == "1":
+        print("Admin active")
+    else:
+        print("Admin inactive")
+else:
+    print("User")    
+        
+    
+    
