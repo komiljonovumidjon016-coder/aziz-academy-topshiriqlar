6ebb57c2b0708a95b03cbc98109797c5717bcb0a
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:08:13 +0500

    Add: 11) Menyu: raqam tanlash [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/11__Menyu__raqam_tanlash.py
@@ -0,0 +1,14 @@
+# n
+# 1 -> One
+# 2 -> Two
+# 3 -> Three
+# aks holda Other
+n = int(input())
+if n == 1:
+    print("One")
+elif n == 2:
+    print("Two")
+elif n == 3:
+    print("Three")
+else:
+    print("Other")
