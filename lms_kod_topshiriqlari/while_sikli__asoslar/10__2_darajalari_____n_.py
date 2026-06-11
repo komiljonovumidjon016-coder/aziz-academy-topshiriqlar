547303bf43a6882b04810695e5f4216f8d78b7fc
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:02:02 +0500

    Add: 10) 2 darajalari (<= n) [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/while_sikli__asoslar/10__2_darajalari_____n_.py
@@ -0,0 +1,8 @@
+# n beriladi.
+# while yordamida 1, 2, 4, 8, ... (2 ning darajalari) n dan oshmaguncha chiqar.
+# Har qatorda bitta.
+n = int(input())
+d = 1
+while d <= n:
+    print(d)
+    d *= 2
