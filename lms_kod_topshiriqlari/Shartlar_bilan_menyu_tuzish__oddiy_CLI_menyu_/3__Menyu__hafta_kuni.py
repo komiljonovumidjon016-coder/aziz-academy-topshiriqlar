3f310a98d1a130a03f942bd9cade75b20f0ae865
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:11:30 +0500

    Add: 3) Menyu: hafta kuni [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/3__Menyu__hafta_kuni.py
@@ -0,0 +1,12 @@
+# n (1..7)
+# 1 -> Mon, 2 -> Tue, 3 -> Wed
+# aks holda Other
+n = int(input())
+if n == 1:
+    print("Mon")
+elif n == 2:
+    print("Tue")
+elif n == 3:
+    print("Wed")
+else:
+    print("Other")
