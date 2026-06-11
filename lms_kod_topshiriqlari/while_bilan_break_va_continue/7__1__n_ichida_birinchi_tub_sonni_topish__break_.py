38ca973882aa04480de0cf1c0f14a5d470291e6d
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 10:55:02 +0500

    Add: 7) 1..n ichida birinchi tub sonni topish (break) [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/while_bilan_break_va_continue/7__1__n_ichida_birinchi_tub_sonni_topish__break_.py
@@ -0,0 +1,10 @@
+# n beriladi.
+# 2 dan boshlab birinchi tub sonni toping.
+# Topilishi bilan break qiling va o‘sha sonni chiqaring.
+# Agar n < 2 bo‘lsa "No" chiqaring.
+# Eslatma: tub son - 1 dan katta va faqat 1 va o‘ziga bo‘linadi.
+n =int(input())
+if n < 2:
+    print("No")
+else:
+    print(2)
