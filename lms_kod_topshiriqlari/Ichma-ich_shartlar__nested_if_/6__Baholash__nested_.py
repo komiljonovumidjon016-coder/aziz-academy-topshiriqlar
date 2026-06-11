d57ab44f6a1b43e1c8fd96fc918a4e18e80703b7
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:18:52 +0500

    Add: 6) Baholash (nested) [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Ichma-ich_shartlar__nested_if_/6__Baholash__nested_.py
@@ -0,0 +1,12 @@
+# score
+# Agar score >= 50 bo'lsa:
+#   agar score >= 80 bo'lsa "Excellent"
+#   aks holda "Pass"
+# Aks holda "Fail"
+score = int(input())
+if score >= 80:
+    print("Excellent")
+elif score >= 80:
+    print("Fail")
+else:
+    print("Pass")
