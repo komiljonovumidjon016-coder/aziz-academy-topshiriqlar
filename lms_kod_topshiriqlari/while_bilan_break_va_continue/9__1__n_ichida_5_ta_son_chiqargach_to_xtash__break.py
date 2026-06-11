28da38a6812bda8c23b6c15803ea68f800f9e314
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 10:54:07 +0500

    Add: 9) 1..n ichida 5 ta son chiqargach to‘xtash (break) [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/while_bilan_break_va_continue/9__1__n_ichida_5_ta_son_chiqargach_to_xtash__break.py
@@ -0,0 +1,12 @@
+# n beriladi.
+# while bilan 1 dan boshlab sonlarni chiqar.
+# 5 ta son chiqargach break qil.
+# Agar n < 5 bo‘lsa, 1..n gacha chiqar.
+n = int(input())
+i = 1
+while i <= n:
+    print(i)
+    if i == 5:
+        break
+    i += 1
+ 
