b13d7b172b73b7d46e92a24c419b8bbe6a12285a
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:28:15 +0500

    Add: 5) Uch holat: musbat/nol/manfiy [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/if__elif__else_asoslari/5__Uch_holat__musbat_nol_manfiy.py
@@ -0,0 +1,12 @@
+# a (butun son)
+# a > 0 -> "Positive"
+# a == 0 -> "Zero"
+# aks holda "Negative"
+a = int(input())
+if a > 0:
+    print("Positive")
+elif a == 0:
+    print("Zero")
+else:
+    print("Negative")
+
