349f5240e78f494e6db86d91f56f691b0e070fe7
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:17:36 +0500

    Add: 3) Nol yoki musbat/manfiy [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Ichma-ich_shartlar__nested_if_/3__Nol_yoki_musbat_manfiy.py
@@ -0,0 +1,12 @@
+# a
+# Agar a == 0 bo'lsa "Zero"
+# Aks holda:
+#   agar a > 0 bo'lsa "Positive"
+#   aks holda "Negative"
+a = int(input())
+if a == 0:
+    print("Zero")
+elif a > 0:
+    print("Positive")
+else:
+    print("Negative")
