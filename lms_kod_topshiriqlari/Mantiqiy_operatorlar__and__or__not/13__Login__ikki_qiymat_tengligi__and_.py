ce9eba4c804d9008289f7dbd1acb2c292f145f0c
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 12:03:17 +0500

    Add: 13) Login: ikki qiymat tengligi (and) [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Mantiqiy_operatorlar__and__or__not/13__Login__ikki_qiymat_tengligi__and_.py
@@ -0,0 +1,5 @@
+# username va password (ikkita qatorda)
+# username == "admin" va password == "1234" bo'lsa True, aks holda False.
+username = input()
+password = input()
+print(username == "admin" and password == "1234")
