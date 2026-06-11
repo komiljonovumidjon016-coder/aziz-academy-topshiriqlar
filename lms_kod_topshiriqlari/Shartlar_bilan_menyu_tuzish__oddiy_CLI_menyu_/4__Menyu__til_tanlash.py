14eea5160dfde46aba7821aa205523775742e6f4
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:11:15 +0500

    Add: 4) Menyu: til tanlash [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/4__Menyu__til_tanlash.py
@@ -0,0 +1,11 @@
+# lang
+# uz -> "Salom"
+# en -> "Hello"
+# aks holda "Unknown language"
+l = input().strip().lower()
+if l == "uz":
+    print("Salom")
+elif l == "en":
+    print("Hello")
+else:
+    print("Unknown language")
