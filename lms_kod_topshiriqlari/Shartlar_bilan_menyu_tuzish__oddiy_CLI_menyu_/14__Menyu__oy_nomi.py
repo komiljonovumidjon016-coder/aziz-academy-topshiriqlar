47f1272d8d1017d026fce2748e9b42361c689744
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:07:11 +0500

    Add: 14) Menyu: oy nomi [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/14__Menyu__oy_nomi.py
@@ -0,0 +1,14 @@
+# m (1..12)
+# 1 -> Jan
+# 2 -> Feb
+# 3 -> Mar
+# aks holda Other
+m = int(input())
+if m == 1:
+    print("Jan")
+elif m == 2:
+    print("Feb")
+elif m == 3:
+    print("Mar")
+else:
+    print("Other")
