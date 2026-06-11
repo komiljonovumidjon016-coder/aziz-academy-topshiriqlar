f10d20b8edb194c2bdfa9f39e34db6a04bbfe050
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:23:11 +0500

    Add: 15) Login + rol [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/if__elif__else_asoslari/15__Login___rol.py
@@ -0,0 +1,9 @@
+# username
+# admin -> "Admin", user -> "User", aks holda "Guest"
+u = input().strip()
+if u == "admin":
+    print("Admin")
+elif u == "user":
+    print("User")
+else:
+    print("Guest")
