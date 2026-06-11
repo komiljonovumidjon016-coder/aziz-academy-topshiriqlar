e6706f6b38d903e85bdc407747fdb3b8e80d4c54
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:12:21 +0500

    Add: 1) Menyu: 1 yoki 2 [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/1__Menyu__1_yoki_2.py
@@ -0,0 +1,11 @@
+# 1 yoki 2 kiriting
+# 1 -> "Hello"
+# 2 -> "Bye"
+# aks holda "Invalid"
+x = int(input())
+if x == 1:
+    print("Hello") 
+elif x == 2:
+    print("Bye")
+else:
+    print("Invalid")
