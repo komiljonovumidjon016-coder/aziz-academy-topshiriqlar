18ee4dc4fcaa864cfd8c201351d3a499dc8b690d
umidjon <komiljonovumidjon016@gmail.com>
2026-06-11 11:01:35 +0500

    Add: 12) 0 kiritsang to‘xtaydi: yig‘indi [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/while_sikli__asoslar/12__0_kiritsang_to_xtaydi__yig_indi.py
@@ -0,0 +1,8 @@
+# Sonlar ketma-ket kiritiladi (har qatorda bitta).
+# 0 kiritilganda to‘xtang va kiritilgan sonlar yig‘indisini chiqaring (0 hisobga olinmaydi).
+y = 0
+s = int(input())
+while s != 0:
+    y += s
+    s = int(input())
+print(y)
