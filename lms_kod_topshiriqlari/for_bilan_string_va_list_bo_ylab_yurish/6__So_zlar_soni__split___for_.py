9ad59a9dd6f9f3b5960d121f593625e1c5251ecf
umidjon <komiljonovumidjon016@gmail.com>
2026-06-12 17:08:42 +0500

    Add: 6) So‘zlar soni (split + for) [komiljonovumidjon016-coder]

---
+++ lms_kod_topshiriqlari/for_bilan_string_va_list_bo_ylab_yurish/6__So_zlar_soni__split___for_.py
@@ -0,0 +1,8 @@
+# Bitta satr matn beriladi.
+# input().split() bilan so‘zlar ro‘yxatini oling.
+# for bilan so‘zlar sonini (len ishlatmasdan) hisoblab chiqaring.
+so = input().split()
+sa = 0
+for soz in so:
+    sa += 1
+print(sa)
